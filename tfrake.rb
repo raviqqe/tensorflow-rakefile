6015d03dadd21bc065673cb8f67731c12ff6bafb
Yota Toyama <raviqqe@gmail.com>
2017-03-20 19:08:54 +0900

    Fix format

--- tfrake.rb
+++ tfrake.rb
@@ -1,7 +1,6 @@
 module TFRake
-  VENV_DIR = '.venv'
-  IN_VENV = ". #{VENV_DIR}/bin/activate &&"
-
+  VENV_DIR = '.venv'.freeze
+  IN_VENV = ". #{VENV_DIR}/bin/activate &&".freeze
 
   def define_tasks(
       module_dir,
@@ -9,48 +8,45 @@ module TFRake
       define_pytest: true,
       pytest_flags: [],
       tensorflow_url: `if which nvidia-smi > /dev/null; then echo tensorflow-gpu; else echo tensorflow; fi`.strip,
-      packages: [])
+      packages: []
+  )
     task :venv do
       sh "#{python} -m venv #{VENV_DIR}" unless File.directory? VENV_DIR
 
       vsh "pip install --upgrade #{[
         tensorflow_url,
-        *%w(pytest pdoc autopep8 twine),
-        *packages,
+        'pytest', 'pdoc', 'autopep8', 'twine',
+        *packages
       ].join ' '}"
 
       vsh 'python setup.py install'
     end
 
-
     task :clean do
       sh 'git clean -dfx'
     end
 
-
-    task_in_venv :pytest do
-      vsh(:pytest, '--doctest-modules', *pytest_flags, module_dir)
-    end if define_pytest
-
+    if define_pytest
+      task_in_venv :pytest do
+        vsh(:pytest, '--doctest-modules', *pytest_flags, module_dir)
+      end
+    end
 
     task_in_venv :pdoc do
       vsh "pdoc --html --html-dir docs --overwrite #{module_dir}"
     end
 
-
     task_in_venv :autopep8 do
       vsh "autopep8 -i #{Dir.glob('**/*.py').join ' '}"
     end
 
-
-    task_in_venv :upload => :test do
+    task_in_venv upload: :test do
       vsh 'python setup.py sdist bdist_wheel'
       vsh 'twine upload dist/*'
     end
   end
 
-
-  def task_in_venv name, &block
+  def task_in_venv(name)
     deps = []
 
     if name.is_a? Hash
@@ -60,13 +56,12 @@ module TFRake
       deps = [deps] unless deps.is_a? Array
     end
 
-    task name => [*%i(clean venv), *deps] do |t|
-      block.call t
+    task name => [:clean, :venv, *deps] do |t|
+      yield t
     end
   end
 
-
-  def vsh *args
-    sh [IN_VENV, *args.map{ |x| x.to_s }].join(' ')
+  def vsh(*args)
+    sh [IN_VENV, *args.map(&:to_s)].join(' ')
   end
 end
